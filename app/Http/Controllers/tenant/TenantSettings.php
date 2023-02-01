12afd47c688ed89f37322f944ce7087d9a7e47b5
Koichiro12 <choiruldhani889@gmail.com>
2023-02-01 11:15:41 +0700

    update

--- app/Http/Controllers/tenant/TenantSettings.php
+++ app/Http/Controllers/tenant/TenantSettings.php
@@ -4,11 +4,37 @@ namespace App\Http\Controllers\tenant;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\tenant\TenantSettings as TenantTenantSettings;
 
 class TenantSettings extends Controller
 {
     //
     public function view_sekolah_settings(){
-        return view('tenant.admin.sekolah.index');
+        $data = TenantTenantSettings::latest()->get();
+        return view('tenant.admin.sekolah.index',compact(['data']));
+    }
+    public function update_sekolah_setting(Request $request){
+        $data_settings = TenantTenantSettings::latest()->get();
+        foreach($data_settings as $data){
+            if($request[$data['settings_name']] != null){
+                $update = TenantTenantSettings::findOrFail($data['id']);
+                $update->update([
+                    'value' => $request[$data['settings_name']],
+                ]);   
+            }
+            if($request->hasFile($data['settings_name'])){
+                $file = $request->file($data['settings_name']);
+                $foto = $file != null ? date('Ymdhis').$file->getClientOriginalName() : $data['value'];
+                if($file != null){
+                    $file->move('public/tenant/upload_file/sekolah',$foto);
+                }
+                $update = TenantTenantSettings::findOrFail($data['id']);
+                $update->update([
+                    'value' => $foto,
+                ]);  
+            }
+        }
+       session()->flash('success',"Sekolah Has Been Updated");
+        return redirect()->route('sekolah');
     }
 }
